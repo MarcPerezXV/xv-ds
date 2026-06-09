b370cbca4742e0cf674250ea56263fcfb4164d46
Marc <154588643+mooooorc@users.noreply.github.com>
2026-06-09 16:13:43 +0200

    error message moved on inputs

--- packages/xv-components/components/input/_base/Base.tsx
+++ packages/xv-components/components/input/_base/Base.tsx
@@ -29,53 +29,52 @@ export const BaseInput = forwardRef<HTMLInputElement, BaseInputProps>(
       [descriptionId, errorId].filter(Boolean).join(" ") || undefined;
 
     return (
-      <div className="xv-text-input">
-        {(label || description) && (
-          <div className="xv-text-input__info">
-            {label && (
-              <label htmlFor={inputId} className="xv-text-input__label">
-                {label}
-              </label>
-            )}
-
-            {description && (
-              <div id={descriptionId} className="xv-text-input__description">
-                {description}
-              </div>
-            )}
-          </div>
+  <div className="xv-text-input">
+    {(label || description) && (
+      <div className="xv-text-input__info">
+        {label && (
+          <label htmlFor={inputId} className="xv-text-input__label">
+            {label}
+          </label>
         )}
-
-        <div className="xv-text-input__control-wrapper">
-          <input
-            ref={ref}
-            id={inputId}
-            disabled={disabled}
-            aria-invalid={!!error}
-            aria-describedby={ariaDescribedBy}
-            className={clsx(
-              "xv-text-input__control",
-              error && "xv-text-input__control--error",
-              className
-            )}
-            {...inputProps}
-          />
-
-          {trailingSlot && (
-            <div className="xv-text-input__trailing">
-              {trailingSlot}
-            </div>
-          )}
-        </div>
-
-        {error && (
-          <div id={errorId} className="xv-text-input__error">
-            <Icon name="xMarkSquare" size="small" />
-            {error}
+        {description && (
+          <div id={descriptionId} className="xv-text-input__description">
+            {description}
           </div>
         )}
       </div>
-    );
+    )}
+
+    {error && (
+      <div id={errorId} className="xv-text-input__error">
+        <Icon name="xMarkSquare" size="small" />
+        {error}
+      </div>
+    )}
+
+    <div className="xv-text-input__control-wrapper">
+      <input
+        ref={ref}
+        id={inputId}
+        disabled={disabled}
+        aria-invalid={!!error}
+        aria-describedby={ariaDescribedBy}
+        className={clsx(
+          "xv-text-input__control",
+          error && "xv-text-input__control--error",
+          className
+        )}
+        {...inputProps}
+      />
+
+      {trailingSlot && (
+        <div className="xv-text-input__trailing">
+          {trailingSlot}
+        </div>
+      )}
+    </div>
+  </div>
+);
   }
 );
 
